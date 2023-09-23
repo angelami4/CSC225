d8a04274d13ab2f8275aa4d259faf1761532f12f
cdavignon <119612059+cdavignon@users.noreply.github.com>
2023-09-22 22:48:34 -0400

    changed font, stroke, and keys used in menu

--- src/Screens/CreditsScreen.java
+++ src/Screens/CreditsScreen.java
@@ -31,25 +31,39 @@ public class CreditsScreen extends Screen {
         // setup graphics on screen (background map, spritefont text)
         background = new TitleScreenMap();
         background.setAdjustCamera(false);
-        creditsLabel = new SpriteFont("CREDITS", 15, 7, "Impact", 30, Color.white);
+        creditsLabel = new SpriteFont("Credits", 15, 7, "Times New Roman", 30, Color.white);
+        creditsLabel.setOutlineColor(Color.black);
+        creditsLabel.setOutlineThickness(3);
         createdByLabel = new SpriteFont("CREATORS:", 150, 121, "Segoe UI", 20, Color.white);
+        createdByLabel.setOutlineColor(Color.black);
+        createdByLabel.setOutlineThickness(3);
         coleLabel =  new SpriteFont("Cole Davignon", 150, 151, "Segoe UI", 20, Color.white);
+        coleLabel.setOutlineColor(Color.black);
+        coleLabel.setOutlineThickness(3);
         angelaLabel =  new SpriteFont("Angela Bruno", 150, 181, "Segoe UI", 20, Color.white);
+        angelaLabel.setOutlineColor(Color.black);
+        angelaLabel.setOutlineThickness(3);
         aidanLabel =  new SpriteFont("Aidan Armellino", 150, 211, "Segoe UI", 20, Color.white);
+        aidanLabel.setOutlineColor(Color.black);
+        aidanLabel.setOutlineThickness(3);
         javiLabel =  new SpriteFont("Javi Reyes", 150, 241, "Segoe UI", 20, Color.white);
-        returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 532, "Times New Roman", 30, Color.white);
-        keyLocker.lockKey(Key.SPACE);
+        javiLabel.setOutlineColor(Color.black);
+        javiLabel.setOutlineThickness(3);
+        returnInstructionsLabel = new SpriteFont("Press Left Arrow to return to the menu", 20, 532, "Times New Roman", 30, Color.white);
+        returnInstructionsLabel.setOutlineColor(Color.black);
+        returnInstructionsLabel.setOutlineThickness(3);
+        keyLocker.lockKey(Key.LEFT);
     }
 
     public void update() {
         background.update(null);
 
-        if (Keyboard.isKeyUp(Key.SPACE)) {
-            keyLocker.unlockKey(Key.SPACE);
+        if (Keyboard.isKeyUp(Key.LEFT)) {
+            keyLocker.unlockKey(Key.LEFT);
         }
 
-        // if space is pressed, go back to main menu
-        if (!keyLocker.isKeyLocked(Key.SPACE) && Keyboard.isKeyDown(Key.SPACE)) {
+        // if LEFT is pressed, go back to main menu
+        if (!keyLocker.isKeyLocked(Key.LEFT) && Keyboard.isKeyDown(Key.LEFT)) {
             screenCoordinator.setGameState(GameState.MENU);
         }
     }
