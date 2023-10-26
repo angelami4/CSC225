b00e995150d96e9168546f6f3585c7cace8f4e34
cdavignon <119612059+cdavignon@users.noreply.github.com>
2023-10-26 17:48:24 -0400

    test

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -19,5 +19,6 @@ public class Game {
         ScreenManager screenManager = gameWindow.getScreenManager(); 
         screenManager.setCurrentScreen(new ScreenCoordinator()); 
         gameWindow.startGame();
+        System.out.println("test");
     } 
 }
